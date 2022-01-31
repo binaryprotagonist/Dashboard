fc61e54505903d3b61562b7ddc1ce60628454b82
binaryprotagonist <binaryprotagonist@gmail.com>
2022-01-31 18:40:22 +0530

    changes Sortable Child data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
-// import ReactTable from './components/ReactTable';
 // import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 
